8dc9c0dbcee91a4960281458cbb89a8a0311f376
Kardach <thomas.kardach@thermofisher.com>
2021-01-26 17:17:15 -0800

    Save Changes

--- src/pages/about-page/AboutPage.js
+++ src/pages/about-page/AboutPage.js
@@ -5,14 +5,15 @@ import React from 'react';
 const AboutPage = ({className}) => {
   return (
     <div className={className}>
-      <h1 className="title">About Me</h1>
+      <img id="about-image"  alt="test" src="https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg"/>
       <div id="about-page">
-        <p style={{float: 'left'}}><img className="about-image"  alt="test" src="https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg"/></p>
-        <p>
-          &emsp;Hi, I'm Tommy, I'm a Software Engineer based in Scotts Valley, California. I am an excellent programmer
-          with experience developing for applications which operate complex machinery. In my free time I enjoy engaging in projects
-          that refine my skills, broaden my knowledge, and teach me valuable lessons which I can take with me to the next project. 
-          When I'm not programming you can find me playing Mario Kart with my wife or playing online Chess. 
+        <p id="about-intro">
+          Hi, I'm Tommy, I'm a Software Engineer based in Scotts Valley, California. 
+        </p>
+        <p id="about-statement">
+          I am an excellent programmer with experience developing for applications which operate complex machinery. 
+          In my free time I enjoy engaging in projects that refine my skills, broaden my knowledge, and teach me 
+          valuable lessons which I can take with me to the next project. 
         </p>
       </div>
     </div>
